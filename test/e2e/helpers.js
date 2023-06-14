66d238884c51cfad7983c11e4ee2c856386202bf
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2023-06-14 13:20:57 +0100

    wip

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -581,7 +581,6 @@ async function unlockWallet(driver, walletPassword) {
   await driver.press('#password', driver.Key.ENTER);
 }
 
-
 function roundToXDecimalPlaces(number, decimalPlaces) {
   return Math.round(number * 10 ** decimalPlaces) / 10 ** decimalPlaces;
 }
